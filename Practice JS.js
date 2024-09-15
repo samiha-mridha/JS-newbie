d5e59af6c13fe83ce57fcefe4c4902b51b8f7796
Samiha Binte Salauddin <samiha.salauddin@gmail.com>
2024-09-15 20:10:17 +0600

    Newbie project of calculator

---
+++ Practice JS.js
@@ -0,0 +1,15 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = (e) => {
+    console.log(e);
+};
+
+
+
+let div = document.querySelector("div");
+div.onmouseover = () => {
+    console.log("you are insane");
+};
+
+
+
